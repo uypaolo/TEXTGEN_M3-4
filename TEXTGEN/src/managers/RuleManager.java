20764a247b2a5eab40cb4032ce9686ae9643ede0
uypaolo <uypaolo@gmail.com>
2013-11-04 00:07:05 +0800

    Rule manager.

---
+++ TEXTGEN/src/managers/RuleManager.java
@@ -0,0 +1,17 @@
+package managers;
+
+import java.util.ArrayList;
+
+import rule.RuleType;
+
+public class RuleManager {
+	
+	private static final String RULE_TYPES_DB_FILE_PATH = "Databases\\RuleTypesDB.xml";
+	
+	public ArrayList<RuleType> getRuleTypes(){
+		ArrayList<RuleType> ruleTypes = new ArrayList<RuleType>();
+		
+		
+		return ruleTypes;
+	}
+}
